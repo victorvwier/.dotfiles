ff19c4b00a5428a74fed6eb7e5d598cdec152231
Victor van Wieringen <victorvwier@hotmail.com>
2024-11-15 11:42:51 +0100

    Add Jellyfin configuration

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,9 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+
+      # Home manager
+      <home-manager/nixos> 
     ];
   
   # Set policy to allow unfree packages
@@ -34,31 +37,27 @@
     ];
   };
 
+  # Home manager
+  home-manager.users.victorvwier = { pkgs, ... }: {
+
+    # DO NOT CHANGE THS NUMBER
+    home.stateVersion = "24.05";
+  };
+
   # System packages
   environment.systemPackages = with pkgs; [
     vim
     steamPackages.steamcmd
     git
     tmux
+    jellyfin
+    jellyfin-web
+    jellyfin-ffmpeg
   ];
 
-  programs.tmux = {
-    enable = true;
-  };
+  # Jellyfin
+  services.jellyfin.enable = true;
   
-  programs.git = {
-    enable = true;
-  };
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
@@ -69,34 +68,12 @@
     };
   };
 
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # Firewall configuration
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ 22 8920 ];
+  networking.firewall.allowedUDPPorts = [ 1900 7359 ];
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  # DO NOT CHANGE THIS NUMBER
   system.stateVersion = "24.05"; # Did you read the comment?
 
 }
